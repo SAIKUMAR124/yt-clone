212e3bfec1dd6bf2885a6c4a4972b90c3cbe0435
dev global <pavankumar.s12459@gmail.com>
2022-06-18 17:20:07 +0530

    Added Sidebar UI & toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
@@ -5,11 +6,15 @@ import HomeScreen from "./screens/homeScreen/HomeScreen";
 import "./_app.scss";
 
 function App() {
+  const [sidebar, toggleSidebar] = useState(false);
+
+  const handleToggleSidebar = () => toggleSidebar((value) => !value);
+
   return (
     <>
-      <Header />
+      <Header handleToggleSidebar={handleToggleSidebar} />
       <div className="app__container border border-info">
-        <Sidebar />
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
         <Container fluid className="app__main border border-warning">
           <HomeScreen />
         </Container>
