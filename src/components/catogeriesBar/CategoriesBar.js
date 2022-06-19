70999c022cbfab1e08478e671dc36c9655753a7e
dev global <pavankumar.s12459@gmail.com>
2022-06-19 17:02:50 +0530

    Added CateegoriesBar functionality

--- src/components/catogeriesBar/CategoriesBar.js
+++ src/components/catogeriesBar/CategoriesBar.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_categoriesBar.scss";
 
+const keywords = [
+  "All",
+  "React Js",
+  "Angular Js",
+  "React Native",
+  "Use of API",
+  "Redux",
+  "Music",
+  "Algorithm art",
+  "Guitar",
+  "Cricket",
+  "Sports",
+  "Football",
+  "Coding",
+  "Hockey",
+  "Power Star Pawan Kalyan",
+];
+
 const CategoriesBar = () => {
-  return <div>CategoriesBar</div>;
+  const [activeElement, setActiveElement] = useState("All");
+
+  const handleClick = (value) => {
+    setActiveElement(value);
+  };
+
+  return (
+    <div className="categoriesBar">
+      {keywords.map((value, index) => (
+        <span key={index} onClick={() => handleClick(value)}
+          className={activeElement === value? 'active': ''}
+        >
+          {value}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
