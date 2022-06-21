8884792d666a3945ef6b4bd2fde4b95dcc064a54
dev global <pavankumar.s12459@gmail.com>
2022-06-21 06:21:09 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,52 @@ import Sidebar from "./components/sidebar/Sidebar";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
 import LoginScreen from "./screens/loginScreen/LoginScreen";
 import "./_app.scss";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
-const App = () => {
+const Layout = ({ children }) => {
   const [sidebar, toggleSidebar] = useState(false);
 
   const handleToggleSidebar = () => toggleSidebar((value) => !value);
-
   return (
-    // <>
-    //   <Header handleToggleSidebar={handleToggleSidebar} />
-    //   <div className="app__container">
-    //     <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
-    //     <Container fluid className="app__main ">
-    //       <HomeScreen />
-    //     </Container>
-    //   </div>
-    // </>
-    <LoginScreen />
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      <div className="app__container">
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+        <Container fluid className="app__main ">
+          {children}
+        </Container>
+      </div>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Layout>
+            <HomeScreen />
+          </Layout>
+        </Route>
+        <Route exact path="/auth">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/search">
+          <Layout>
+            <h1>Search Results</h1>
+          </Layout>
+        </Route>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
